9021fb1e267f23daa0fef4e1250123a22ecaa807
zeinab khaled <zezekhaled13@gmail.com>
2025-04-07 01:21:55 +0200

    modify user router

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -4,6 +4,7 @@ import { isValid } from "../../middleware/validation.js";
 import { forgetpassVal } from "./user.validation.js";
 import { forgetPassword,  resetPassword,  } from "./user.controller.js";
 import { asyncHandler } from "../../middleware/asyncHandler.js";
+import { isAuthenticated } from "../../middleware/authentication.js";
 
 // router
 const userRouter= Router()
@@ -12,5 +13,5 @@ const userRouter= Router()
 userRouter.post('/forget',isValid(forgetpassVal),asyncHandler(forgetPassword))
 
 // reset password
-userRouter.put('/reset',asyncHandler(resetPassword))
+userRouter.put('/reset', isAuthenticated(), asyncHandler(resetPassword))
 export default userRouter
