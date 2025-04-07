9a033f5f9482c36e4416a74b8a0a0266ddea79e1
zeinab khaled <zezekhaled13@gmail.com>
2025-04-08 01:42:32 +0200

    api read file

--- src/modules/doctor/doctor..controller.js
+++ src/modules/doctor/doctor..controller.js
@@ -1,3 +1,4 @@
+import XLSX from "xlsx";
 import { Doctor } from "../../../db/index.js"
 import cloudinary from "../../utilies/cloud.js"
 
@@ -12,3 +13,31 @@ export const getAllDoctor = async (req,res,next) => {
     // send response
     return res.status(200).json({data: allDoctor, success: true})
 }
+
+// read file
+export const extractData = async (req, res, next) => {
+  const file = req.file;
+  const filePath = file.path;
+  const workbook = XLSX.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
+  const jsonData = XLSX.utils.sheet_to_json(worksheet);
+  for (const ele of jsonData) {
+    const doctor = new Doctor({
+      fullname: ele["Full Name"],
+      price: ele["Price"],
+      name: ele["Name"],
+      jobTitle: ele["Job Title"],
+      bookingInfo: {
+        from: ele["Booking info"],
+        info: ele["Booking Info 1"],
+      },
+      visitorRate: ele["Visitors"],
+      doctorInfo: ele["Doctor Info"],
+      locations: [ele["Location "], ele["Location 1"], ele["Location 2"]],
+    });
+    await doctor.save();
+  }
+  const data = await Doctor.find();
+  res.json({ data });
+};
--- src/modules/doctor/doctor.router.js
+++ src/modules/doctor/doctor.router.js
@@ -3,17 +3,20 @@ import { Router } from "express";
 import { isAuthenticated } from "../../middleware/authentication.js";
 import { cloudUploads } from "../../utilies/multer_cloud.js";
 import { asyncHandler } from "../../middleware/asyncHandler.js";
-import { getAllDoctor } from "./doctor..controller.js";
+import { extractData } from "./doctor..controller.js";
+import { fileUploads } from "../../utilies/multer.js";
 
 // doctor router
 const doctorRouter = Router()
 
 // get all doctor
-doctorRouter.get(
-    '/getAllDoctor',
-     isAuthenticated(),
-      cloudUploads().single('file'),
-       asyncHandler(getAllDoctor)
-    )
+// doctorRouter.get(
+//     '/getAllDoctor',
+//      isAuthenticated(),
+//       cloudUploads().single('file'),
+//        asyncHandler(getAllDoctor)
+//     )
+
+doctorRouter.post('/read-file',fileUploads().single('file'), asyncHandler(extractData))
 
 export default doctorRouter
