e65f5536223c828ea635893619cd6422e22f2654
zeinab khaled <zezekhaled13@gmail.com>
2025-04-08 19:58:39 +0200

    modify doctor model

--- db/models/doctor.model.js
+++ db/models/doctor.model.js
@@ -1,35 +1,6 @@
 import { model, Schema } from "mongoose";
 
 // schema 
-const doctorSchema = new Schema({
-    // fullname: {
-    //     type: String,
-    //     required: true,
-    //     trim: true
-    // },
-    // name: {
-    //     type: String,
-    //     required: true,
-    //     trim: true
-    // },
-    // price: {
-    //     type: String,
-    //     required: true,
-    //     trim: true
-    // },
-    // jobTitle: {
-    //     type: String,
-    //     required: true,
-    //     trim: true
-    // },
-    // bookingInfo: {
-    //     info:{
-    //         type: String,
-    //     required: true,
-    //     trim: true
-    //     }
-    // }
-
-})
+const doctorSchema = new Schema({})
 // model
 export const Doctor = model('Doctor', doctorSchema)
