fd68bcc60a6fa264a09194b61e307925265e47f5
zeinab khaled <zezekhaled13@gmail.com>
2025-04-08 02:13:35 +0200

    modify multer

--- src/utilies/multer.js
+++ src/utilies/multer.js
@@ -7,7 +7,7 @@ import { AppError } from './appError.js';
 
 // file validation
 export const fileValidation = {
-    file: ['application/vnd.ms-excel']
+    file: ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']
 }
 
 // fileUpload
