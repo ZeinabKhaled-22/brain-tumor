8f3bc870cbeadab6d862d9043bd2c052df446acf
zeinab khaled <zezekhaled13@gmail.com>
2025-04-10 00:39:18 +0200

    modify user data

--- db/models/userData.model.js
+++ db/models/userData.model.js
@@ -12,11 +12,18 @@ const userDataSchema = new Schema({
         enum: Object.values(gender)
     },
     dateOfBirth: Date,
-    bodyMeasurement: {
-        height: Number,
-        width: Number,
-        finalResult: Number
+     bodyMeasurement:{
+         type: {
+        height: { type: Number, required: true },
+        width: { type: Number, required: true },
+        finalresult: { type: Number, required: true },
+      }
     },
+    //[{
+    //     height: Number,
+    //     width: Number,
+    //     finalResult: Number
+    // }],
     healthCondition: {
         type: String,
         enum: Object.values(healthCondition)
