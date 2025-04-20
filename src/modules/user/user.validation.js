59f282fd42dfc423b04d4e0b118471633e4d8238
zeinab khaled <zezekhaled13@gmail.com>
2025-04-20 05:02:26 +0200

    edit

--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -15,5 +15,5 @@ export const editProfileVal = joi.object({
 
 // change email
 export const changeEmailVal = joi.object({
-  email: generalFields.newEmail.required()
+  newEmail: generalFields.newEmail.required()
 })
