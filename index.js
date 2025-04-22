15cd2322e4b4edeefa8dbc307348a0231a04259a
zeinab khaled <zezekhaled13@gmail.com>
2025-04-23 00:15:40 +0200

    signup with goggle

--- index.js
+++ index.js
@@ -18,6 +18,26 @@ connectDB()
 // bootStrap
 bootStrap(app,express)
 
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+  );
+  
+  app.get('/auth/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+      // successful login
+      res.redirect('/profile');
+    }
+  );
+  
+  app.get("/profile", (req, res) => {
+    if (!req.user) {
+      return res.redirect("/auth/google");
+    }
+    res.send(`<h1>Welcome ${req.user.displayName}</h1>`);
+  });
+  
+
 // listen
 app.listen(port, () => {
     console.log('server is running on port',port);
